f10237c19ee45de0b82132f2fed1e5dfc296bf56
johnny yip <johnnyyip427@gmail.com>
2018-10-30 23:24:40 -0700

    added functionality CRUD to app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926004733) do
+ActiveRecord::Schema.define(version: 20181023024406) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "item"
+    t.string "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
